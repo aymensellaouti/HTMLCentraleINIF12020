d9c4cf45d59c50d1d64653c7f25a909159b756d7
aymen <aymen.sellaouti@gmail.com>
2020-02-20 13:42:56 +0100

    exemple setTimout

---
+++ assets/js/script2.js
@@ -0,0 +1,14 @@
+function sayHello() {
+    console.log('Salut');
+}
+setTimeout( () => {
+    console.log('Salut 2');
+}, 2500);
+console.log('JE suis synchrone');
+i = 5;
+indice = setInterval(() => {
+    console.log(i--);
+    if(i == 0) {
+        clearInterval(indice);
+    }
+}, 1000);
